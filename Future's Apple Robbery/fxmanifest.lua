2d503a6b1540004129add18c2cffae32587e4f93
awesoemgaming <129987735+awesoemgaming@users.noreply.github.com>
2023-05-08 20:51:42 -0700

    Create fxmanifest.lua

---
+++ Future's Apple Robbery/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Awesome Gaming'
+description 'Apple store Robbery For QBCore.'
+version '2.0.0'
+
+client_scripts {
+    'disp1/display1.lua',
+    'disp2/display2.lua',
+    'disp3/display3.lua',
+    'disp4/didipslay4.lua',
+    'disp5/display5.lua'
+}
+
+server_scripts {
+    'server.lua.lua'
+}
